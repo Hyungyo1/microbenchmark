429945b539d3bb4969c4eb1fd2f3ed989e829114
hyungyo1 <hyungyo95@gmail.com>
2024-10-09 13:22:06 -0500

    update

---
+++ cpu_mbm.py
@@ -0,0 +1,98 @@
+from numa_alloc import numa_alloc_tensor, numa_free_tensor
+import torch
+import time
+import argparse
+import numpy as np
+import os
+import psutil
+
+def realloc_to_numa(tensor):
+    numa_tensor = numa_alloc_tensor(tensor.shape, tensor.dtype)
+    if numa_tensor is not None:
+        numa_tensor.copy_(tensor)
+        del tensor
+        return numa_tensor
+    else:
+        raise MemoryError("Fail to allocate CXL memory!")
+
+# Argument parsing
+parser = argparse.ArgumentParser(description="Matrix multiplication with optional settings.")
+parser.add_argument('--bmm', action='store_true', help="Use batched matrix multiplication.")
+parser.add_argument('--amx', action='store_true', help="Use AMX (Advanced Matrix Extensions).")
+parser.add_argument('--bsz', type=int, default=5040, help="Batch size (only used if --bmm is set).")
+parser.add_argument('--m', type=int, default=2048, help="Number of rows of the first matrix.")
+parser.add_argument('--n', type=int, default=2048, help="Number of columns of the first matrix and rows of the second matrix.")
+parser.add_argument('--k', type=int, default=2048, help="Number of columns of the second matrix.")
+parser.add_argument('--iter', type=int, default=5, help="Number of iterations to repeat the process.")
+parser.add_argument('--warmup', type=int, default=2, help="Number of iterations to repeat the process.")
+parser.add_argument('--cxl', action='store_true', help="Use CXL to store Param/KV cache.")
+args = parser.parse_args()
+
+# Set variables based on arguments
+bmm = args.bmm
+amx = args.amx
+bsz = args.bsz if bmm else 1
+m = args.m
+n = args.n
+k = args.k
+iterations = args.iter
+warmup = args.warmup
+cxl = args.cxl
+
+data_type = torch.bfloat16 if amx else torch.float16
+
+# p = psutil.Process(os.getpid())
+# p.cpu_affinity([40])
+
+# Repeat the time measurement process
+duration_list = []
+for i in range(iterations):
+    # Generate random tensors
+    if bmm:
+        a = torch.rand(bsz, m, n).to(data_type)
+        b = torch.rand(bsz, n, k).to(data_type)
+    else:
+        a = torch.rand(m, n).to(data_type)
+        b = torch.rand(n, k).to(data_type)
+
+    if cxl:
+        b = realloc_to_numa(b)
+
+    # Measure time
+
+    if amx:
+        with torch.cpu.amp.autocast():
+            if bmm:
+                start = time.time()
+                c = torch.bmm(a, b)
+                end = time.time()
+            else:
+                start = time.time()
+                c = torch.matmul(a, b)
+                end = time.time()
+    else:
+        with torch.backends.mkldnn.verbose(torch.backends.mkldnn.VERBOSE_ON):
+
+            if bmm:
+                start = time.time()
+                c = torch.bmm(a, b)
+                end = time.time()
+            else:
+                start = time.time()
+                c = torch.matmul(a, b)
+                end = time.time()
+
+    # Accumulate duration
+    duration = end - start
+    if i > warmup - 1:
+        duration_list.append(duration)
+
+        print(f"Iteration {i - warmup}: output data type: {c.dtype}, Duration: {duration:.6f} seconds")
+
+# Calculate average duration and GFLOPS
+average_duration = np.median(duration_list)
+n_comp = 2 * bsz * m * n * k if bmm else 2 * m * n * k
+gflops = n_comp / average_duration / 10**9
+
+print(f"Average Duration: {average_duration:.6f} seconds")
+print(f"Throughput: {gflops} (GFLOPS)")
--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
-python3 gpu_mbm.py --m 64 --n 12288 --k 49152 --iter 20 --warmup 5 >> out-gemm-64.txt
-python3 gpu_mbm.py --m 512 --n 12288 --k 49152 --iter 20 --warmup 5 >> out-gemm-512.txt
-python3 gpu_mbm.py --m 4096 --n 12288 --k 49152 --iter 20 --warmup 5 >> out-gemm-4096.txt
-python3 gpu_mbm.py --m 36864 --n 12288 --k 49152 --iter 20 --warmup 5 >> out-gemm-36864.txt
-python3 gpu_mbm.py --bmm --bsz 96 --m 1 --n 128 --k 64 --iter 20 --warmup 5 >> out-bmm-1-64.txt
-python3 gpu_mbm.py --bmm --bsz 96 --m 1 --n 128 --k 256 --iter 20 --warmup 5 >> out-bmm-1-256.txt
-python3 gpu_mbm.py --bmm --bsz 96 --m 1 --n 128 --k 1024 --iter 20 --warmup 5 >> out-bmm-1-1024.txt
-python3 gpu_mbm.py --bmm --bsz 3072 --m 1 --n 128 --k 64 --iter 20 --warmup 5 >> out-bmm-32-64.txt
-python3 gpu_mbm.py --bmm --bsz 3072 --m 1 --n 128 --k 256 --iter 20 --warmup 5 >> out-bmm-32-256.txt
-python3 gpu_mbm.py --bmm --bsz 3072 --m 1 --n 128 --k 1024 --iter 20 --warmup 5 >> out-bmm-32-1024.txt
+OMP_NUM_THREADS=40 numactl -m 0 -C 0-39 python cpu_mbm.py --amx --m 64 --n 12288 --k 49152 --iter 15 --warmup 5 &> ./data/out-gemm-64.txt
+OMP_NUM_THREADS=40 numactl -m 0 -C 0-39 python cpu_mbm.py --amx --m 512 --n 12288 --k 49152 --iter 15 --warmup 5 &> ./data/out-gemm-512.txt
+OMP_NUM_THREADS=40 numactl -m 0 -C 0-39 python cpu_mbm.py --amx --m 4096 --n 12288 --k 49152 --iter 15 --warmup 5 &> ./data/out-gemm-4096.txt
+OMP_NUM_THREADS=40 numactl -m 0 -C 0-39 python cpu_mbm.py --amx --m 36864 --n 12288 --k 49152 --iter 15 --warmup 5 &> ./data/out-gemm-36864.txt
+OMP_NUM_THREADS=40 numactl -m 0 -C 0-39 python cpu_mbm.py --amx --bmm --bsz 96 --m 1 --n 128 --k 64 --iter 15 --warmup 5 &> ./data/out-bmm-1-64.txt
+OMP_NUM_THREADS=40 numactl -m 0 -C 0-39 python cpu_mbm.py --amx --bmm --bsz 96 --m 1 --n 128 --k 256 --iter 15 --warmup 5 &> ./data/out-bmm-1-256.txt
+OMP_NUM_THREADS=40 numactl -m 0 -C 0-39 python cpu_mbm.py --amx --bmm --bsz 96 --m 1 --n 128 --k 1024 --iter 15 --warmup 5 &> ./data/out-bmm-1-1024.txt
+OMP_NUM_THREADS=40 numactl -m 0 -C 0-39 python cpu_mbm.py --amx --bmm --bsz 3072 --m 1 --n 128 --k 64 --iter 15 --warmup 5 &> ./data/out-bmm-32-64.txt
+OMP_NUM_THREADS=40 numactl -m 0 -C 0-39 python cpu_mbm.py --amx --bmm --bsz 3072 --m 1 --n 128 --k 256 --iter 15 --warmup 5 &> ./data/out-bmm-32-256.txt
+OMP_NUM_THREADS=40 numactl -m 0 -C 0-39 python cpu_mbm.py --amx --bmm --bsz 3072 --m 1 --n 128 --k 1024 --iter 15 --warmup 5 &> ./data/out-bmm-32-1024.txt
