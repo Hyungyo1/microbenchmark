c4a4ad11c2537589a581cd8218622b974f9f4d2c
hyungyo1 <hyungyo95@gmail.com>
2024-10-14 03:10:49 -0500

    Flexgen scripts updated

--- flexgen_scripts/flexgen_175b.sh
+++ flexgen_scripts/flexgen_175b.sh
@@ -1,16 +1,16 @@
 ##Online
 
-OMP_NUM_THREADS=40 numactl -m 0,1 -C 0-39 python -m flexgen.flex_opt --model facebook/opt-175b --path _DUMMY_ --percent 24 76 0 100 0 100 --gpu-batch-size 1 --num-gpu-batches 1 --prompt-len 32 --gen-len 32 --offload-dir ./ --cpu-cache-compute --pin-weight 0 &> "./opt175b_32_32_b=1.log"
+OMP_NUM_THREADS=40 numactl -m 0,1 -C 0-39 python -m flexgen.flex_opt --model facebook/opt-175b --path _DUMMY_ --percent 22 78 100 0 100 0 --gpu-batch-size 1 --num-gpu-batches 1 --prompt-len 32 --gen-len 32 --offload-dir ./ --cpu-cache-compute --pin-weight 0 &> "./opt175b_32_32_b=1.log"
 
-OMP_NUM_THREADS=40 numactl -m 0,1 -C 0-39 python -m flexgen.flex_opt --model facebook/opt-175b --path _DUMMY_ --percent 22 78 0 100 0 100 --gpu-batch-size 1 --num-gpu-batches 1 --prompt-len 256 --gen-len 32 --offload-dir ./ --cpu-cache-compute --pin-weight 0 &> "./opt175b_256_32_b=1.log"
+OMP_NUM_THREADS=40 numactl -m 0,1 -C 0-39 python -m flexgen.flex_opt --model facebook/opt-175b --path _DUMMY_ --percent 20 80 100 0 100 0 --gpu-batch-size 1 --num-gpu-batches 1 --prompt-len 256 --gen-len 32 --offload-dir ./ --cpu-cache-compute --pin-weight 0 &> "./opt175b_256_32_b=1.log"
 
-OMP_NUM_THREADS=40 numactl -m 0,1 -C 0-39 python -m flexgen.flex_opt --model facebook/opt-175b --path _DUMMY_ --percent 20 80 0 100 0 100 --gpu-batch-size 1 --num-gpu-batches 1 --prompt-len 2016 --gen-len 32 --offload-dir ./ --cpu-cache-compute --pin-weight 0 &> "./opt175b_2016_32_b=1.log" 
+OMP_NUM_THREADS=40 numactl -m 0,1 -C 0-39 python -m flexgen.flex_opt --model facebook/opt-175b --path _DUMMY_ --percent 18 82 100 0 100 0 --gpu-batch-size 1 --num-gpu-batches 1 --prompt-len 2016 --gen-len 32 --offload-dir ./ --cpu-cache-compute --pin-weight 0 &> "./opt175b_2016_32_b=1.log" 
 
-OMP_NUM_THREADS=40 numactl -m 0,1 -C 0-39 python -m flexgen.flex_opt --model facebook/opt-175b --path _DUMMY_ --percent 22 78 0 100 0 100 --gpu-batch-size 1 --num-gpu-batches 1 --prompt-len 32 --gen-len 256 --offload-dir ./ --cpu-cache-compute --pin-weight 0 &> "./opt175b_32_256_b=1.log"
+OMP_NUM_THREADS=40 numactl -m 0,1 -C 0-39 python -m flexgen.flex_opt --model facebook/opt-175b --path _DUMMY_ --percent 20 80 100 0 100 0 --gpu-batch-size 1 --num-gpu-batches 1 --prompt-len 32 --gen-len 256 --offload-dir ./ --cpu-cache-compute --pin-weight 0 &> "./opt175b_32_256_b=1.log"
 
-OMP_NUM_THREADS=40 numactl -m 0,1 -C 0-39 python -m flexgen.flex_opt --model facebook/opt-175b --path _DUMMY_ --percent 20 80 0 100 0 100 --gpu-batch-size 1 --num-gpu-batches 1 --prompt-len 256 --gen-len 256 --offload-dir ./ --cpu-cache-compute --pin-weight 0 &> "./opt175b_256_256_b=1.log"
+OMP_NUM_THREADS=40 numactl -m 0,1 -C 0-39 python -m flexgen.flex_opt --model facebook/opt-175b --path _DUMMY_ --percent 18 82 100 0 100 0 --gpu-batch-size 1 --num-gpu-batches 1 --prompt-len 256 --gen-len 256 --offload-dir ./ --cpu-cache-compute --pin-weight 0 &> "./opt175b_256_256_b=1.log"
 
-OMP_NUM_THREADS=40 numactl -m 0,1 -C 0-39 python -m flexgen.flex_opt --model facebook/opt-175b --path _DUMMY_ --percent 20 80 0 100 0 100 --gpu-batch-size 1 --num-gpu-batches 1 --prompt-len 1792 --gen-len 256 --offload-dir ./ --cpu-cache-compute --pin-weight 0 &> "./opt175b_1792_256_b=1.log"
+OMP_NUM_THREADS=40 numactl -m 0,1 -C 0-39 python -m flexgen.flex_opt --model facebook/opt-175b --path _DUMMY_ --percent 16 84 100 0 100 0 --gpu-batch-size 1 --num-gpu-batches 1 --prompt-len 1792 --gen-len 256 --offload-dir ./ --cpu-cache-compute --pin-weight 0 &> "./opt175b_1792_256_b=1.log"
 
 ##Offline
 
--- flexgen_scripts/flexgen_66b.sh
+++ flexgen_scripts/flexgen_66b.sh
@@ -1,16 +1,16 @@
 ##Online
 
-OMP_NUM_THREADS=40 numactl -m 0,1 -C 0-39 python -m flexgen.flex_opt --model facebook/opt-66b --path _DUMMY_ --percent 57 43 0 100 0 100 --gpu-batch-size 1 --num-gpu-batches 1 --prompt-len 32 --gen-len 32 --offload-dir ./ --cpu-cache-compute --pin-weight 0 &> "./opt66b_32_32_b=1.log"
+OMP_NUM_THREADS=40 numactl -m 0,1 -C 0-39 python -m flexgen.flex_opt --model facebook/opt-66b --path _DUMMY_ --percent 55 45 100 0 100 0 --gpu-batch-size 1 --num-gpu-batches 1 --prompt-len 32 --gen-len 32 --offload-dir ./ --cpu-cache-compute --pin-weight 0 &> "./opt66b_32_32_b=1.log"
 
-OMP_NUM_THREADS=40 numactl -m 0,1 -C 0-39 python -m flexgen.flex_opt --model facebook/opt-66b --path _DUMMY_ --percent 55 45 0 100 0 100 --gpu-batch-size 1 --num-gpu-batches 1 --prompt-len 256 --gen-len 32 --offload-dir ./ --cpu-cache-compute --pin-weight 0 &> "./opt66b_256_32_b=1.log"
+OMP_NUM_THREADS=40 numactl -m 0,1 -C 0-39 python -m flexgen.flex_opt --model facebook/opt-66b --path _DUMMY_ --percent 53 47 100 0 100 0 --gpu-batch-size 1 --num-gpu-batches 1 --prompt-len 256 --gen-len 32 --offload-dir ./ --cpu-cache-compute --pin-weight 0 &> "./opt66b_256_32_b=1.log"
 
-OMP_NUM_THREADS=40 numactl -m 0,1 -C 0-39 python -m flexgen.flex_opt --model facebook/opt-66b --path _DUMMY_ --percent 50 50 0 100 0 100 --gpu-batch-size 1 --num-gpu-batches 1 --prompt-len 2016 --gen-len 32 --offload-dir ./ --cpu-cache-compute --pin-weight 0 &> "./opt66b_2016_32_b=1.log" 
+OMP_NUM_THREADS=40 numactl -m 0,1 -C 0-39 python -m flexgen.flex_opt --model facebook/opt-66b --path _DUMMY_ --percent 50 50 100 0 100 0 --gpu-batch-size 1 --num-gpu-batches 1 --prompt-len 2016 --gen-len 32 --offload-dir ./ --cpu-cache-compute --pin-weight 0 &> "./opt66b_2016_32_b=1.log" 
 
-OMP_NUM_THREADS=40 numactl -m 0,1 -C 0-39 python -m flexgen.flex_opt --model facebook/opt-66b --path _DUMMY_ --percent 55 45 0 100 0 100 --gpu-batch-size 1 --num-gpu-batches 1 --prompt-len 32 --gen-len 256 --offload-dir ./ --cpu-cache-compute --pin-weight 0 &> "./opt66b_32_256_b=1.log"
+OMP_NUM_THREADS=40 numactl -m 0,1 -C 0-39 python -m flexgen.flex_opt --model facebook/opt-66b --path _DUMMY_ --percent 53 47 100 0 100 0 --gpu-batch-size 1 --num-gpu-batches 1 --prompt-len 32 --gen-len 256 --offload-dir ./ --cpu-cache-compute --pin-weight 0 &> "./opt66b_32_256_b=1.log"
 
-OMP_NUM_THREADS=40 numactl -m 0,1 -C 0-39 python -m flexgen.flex_opt --model facebook/opt-66b --path _DUMMY_ --percent 52 48 0 100 0 100 --gpu-batch-size 1 --num-gpu-batches 1 --prompt-len 256 --gen-len 256 --offload-dir ./ --cpu-cache-compute --pin-weight 0 &> "./opt66b_256_256_b=1.log"
+OMP_NUM_THREADS=40 numactl -m 0,1 -C 0-39 python -m flexgen.flex_opt --model facebook/opt-66b --path _DUMMY_ --percent 50 50 100 0 100 0 --gpu-batch-size 1 --num-gpu-batches 1 --prompt-len 256 --gen-len 256 --offload-dir ./ --cpu-cache-compute --pin-weight 0 &> "./opt66b_256_256_b=1.log"
 
-OMP_NUM_THREADS=40 numactl -m 0,1 -C 0-39 python -m flexgen.flex_opt --model facebook/opt-66b --path _DUMMY_ --percent 50 50 0 100 0 100 --gpu-batch-size 1 --num-gpu-batches 1 --prompt-len 1792 --gen-len 256 --offload-dir ./ --cpu-cache-compute --pin-weight 0 &> "./opt66b_1792_256_b=1.log"
+OMP_NUM_THREADS=40 numactl -m 0,1 -C 0-39 python -m flexgen.flex_opt --model facebook/opt-66b --path _DUMMY_ --percent 50 50 100 0 100 0 --gpu-batch-size 1 --num-gpu-batches 1 --prompt-len 1792 --gen-len 256 --offload-dir ./ --cpu-cache-compute --pin-weight 0 &> "./opt66b_1792_256_b=1.log"
 
 ##Offline
 
